abd1798310e4b03799644d1cbf1f1696b51367b2
batinorene <batinorene@gmail.com>
2024-04-25 08:01:08 +0000

    stling

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ const port = process.argv[3] || 3000;
 app.use(bodyParser.urlencoded({extended:true}))
     .set("views",__dirname+'/views')
     .set("view engine","ejs");
-
+    
     app.engine("html",require("ejs").renderFile);
     
 require('./routes/main')(app);
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.listen(port,()=> console.log(`example app listening on http://localhost:${port}`));
